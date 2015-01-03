81239e307f08fa5253ea73c045c1f630da67b892
Bill <wcharlesm@gmail.com>
2015-01-02 23:24:16 -0500

    Update dbconfig.php

--- php/config/dbconfig.php
+++ php/config/dbconfig.php
@@ -1,13 +1,13 @@
 <?php
 //-----------------------------------------
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+defined('DB_SERVER') ? null : define("DB_SERVER", "########");
 
-defined('DB_NAME') ? null : define("DB_NAME", "resume");
+defined('DB_NAME') ? null : define("DB_NAME", "########");
 
-defined('DB_USER') ? null : define("DB_USER", "wmagee");
+defined('DB_USER') ? null : define("DB_USER", "########");
 
-defined('DB_PASS') ? null : define("DB_PASS", "gs8a*is#");
+defined('DB_PASS') ? null : define("DB_PASS", "########");
 
-defined('DB_PORT') ? null : define("DB_PORT", "3306");
+defined('DB_PORT') ? null : define("DB_PORT", "########");
 //-----------------------------------------
 ?>
